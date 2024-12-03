a7650bde6b6d11aea589569d6379e0c0a5edbbb8
johan <164965803+J0hANGiL@users.noreply.github.com>
2024-12-02 20:29:54 -0500

    implementando y probada la funcionalidad para inhablitar usuarios

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -7,4 +7,98 @@ ruta.get('/', (req,res)=>{
     res.json('respuesta a peticion GET de USUARIOS funcionando correctamente...');
 });
 
+//validaciones para el objeto usuario
+const schema = Joi.object({
+    nombre: Joi.string()
+    .min(3)
+    .max(30)
+    .required()
+    .pattern(/^[A-Za-záéíóú ]{3,30}$/),
+
+    password: Joi.string()
+    .pattern(/^[a-zA-Z0-9]{3,30}$/),
+
+    email: Joi.string()
+    .email({minDomainSegments: 2, tlds: { allow: ['com', 'net', 'edu', 'co'] } }) 
+});
+
+//Endpoint de tipo POST para el recurso USUARIOS
+ruta.post('/', (req,res)=>{
+    let body = req.body;
+
+    const {error, value} = schema.validate({nombre: body.nombre, email: body.email});
+    if(!error){
+        let resultado = crearUsuario(body);
+
+        resultado.then( user =>{
+            res.json({
+                valor: user
+            })
+        }).catch( err =>{
+            res.status(400).json({
+                err
+            })
+        });
+    }else{
+        res.status(400).json({
+            error
+        })
+    }
+});
+
+//Funcion asincrona para crear un objeto de tipo usuario
+async function crearUsuario(body){
+    let usuario = new Usuario({
+        email     : body.email,
+        nombre    : body.nombre,
+        password  : body.password
+    });
+    return await usuario.save();
+}
+
+//endpoint de tipo PUT para actualizar los datos del usuario
+ruta.put('/:email', (req, res) => {
+    const {error, value} = schema.validate({nombre: req.body.nombre });
+    if(!error){
+        let resultado = actualizarUsuario(req.params.email, req.body);
+        resultado.then(valor =>{
+            res.json({
+                valor
+            })
+        }).catch(err => {
+            res.status(400).json({
+                err
+            })
+        });
+    }else{
+        res.status(400).json({
+            error
+        })
+    }
+});
+
+async function actualizarUsuario(email, body){
+    let usuario = await Usuario.findOneAndUpdate({"email": email}, {
+        $set: {
+            nombre : body.nombre,
+            password : body.password
+        }
+    },{new: true});
+    return usuario;
+}
+
+//endpoint de tipo delete para el recurso USUARIOS 
+ruta.delete('/:email', (req, res) => {
+        let resultado = desactivarUsuario(req.params.email);
+        resultado.then(valor =>{
+            res.json({
+                valor
+            })
+        }).catch(err => {
+            res.status(400).json({
+                err
+            })
+        });
+});
+
 module.exports = ruta;
